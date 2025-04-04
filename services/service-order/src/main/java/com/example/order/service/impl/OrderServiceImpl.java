ef97fec86635877a2d9a51af3e455d58a9b4b741
lingzhexi <lingzhexi@gmail.com>
2025-04-04 21:09:30 +0800

    sentinel 异常处理 Sphu 硬编码

--- services/service-order/src/main/java/com/example/order/service/impl/OrderServiceImpl.java
+++ services/service-order/src/main/java/com/example/order/service/impl/OrderServiceImpl.java
@@ -1,20 +1,21 @@
 package com.example.order.service.impl;
+
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.example.order.bean.Order;
 import com.example.order.feign.ProductFeignClient;
 import com.example.order.service.OrderService;
 import com.example.product.bean.Product;
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.List;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 
 @Slf4j
@@ -34,14 +35,22 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public Order createOrder(Long productId, Long userId) {
 //        Product product = getProduct(productId);
+
+        //使用Feign完成远程调用
         Product product = productFeignClient.getProductById(productId);
         Order order = new Order();
         order.setId(1L);
         order.setTotalAmount(product.getPrice().multiply(new BigDecimal(product.getNum())));
         order.setUserId(userId);
-        order.setNickName("lingzx");
+        order.setNickName("stormling");
         order.setAddress("恒大城");
         order.setProductList(Arrays.asList(product));
+//        try (Entry entry = SphU.entry("resourceName")){
+//            //被保护的业务逻辑
+//        } catch (BlockException e) {
+//            //资源访问阻止，被限流或被降级
+//            log.info("资源被限制了");
+//        }
         return order;
     }
 
@@ -56,7 +65,6 @@ public class OrderServiceImpl implements OrderService {
         return order;
     }
 
-
     private Product getProductFromRemote(Long productId) {
         List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
         ServiceInstance serviceInstance = instances.get(0);
@@ -65,7 +73,6 @@ public class OrderServiceImpl implements OrderService {
 
         Product product = restTemplate.getForObject(url, Product.class);
         return product;
-
     }
 
     private Product getProduct(Long productId) {
