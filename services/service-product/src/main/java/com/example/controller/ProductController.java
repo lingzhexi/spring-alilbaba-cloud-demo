761d3389eee1a8e398f273775657dbaf9059dff8
lingzhexi <lingzhexi@gmail.com>
2025-04-07 02:21:18 +0800

    熔断降级 慢调用比例

--- services/service-product/src/main/java/com/example/controller/ProductController.java
+++ services/service-product/src/main/java/com/example/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import com.example.product.bean.Product;
 import com.example.service.ProductService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,14 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
+    @SneakyThrows
     @GetMapping("/product/{id}")
     public Product getProduct(@PathVariable("id")Long productId,
                               HttpServletRequest request) {
         String header = request.getHeader("X-Token");
         System.out.println(header);
         System.out.println("welcome");
+//        TimeUnit.SECONDS.sleep(2000);
         return productService.getProduct(productId);
     }
 
