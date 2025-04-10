48e1e2e9f32866860d8925b00a119a805ded4d8e
Geek_Fox <lingzhexi2022@126.com>
2025-04-10 11:53:17 +0800

    网关-过滤器：路径重写和响应头添加参数

--- services/service-product/src/main/java/com/example/controller/ProductController.java
+++ services/service-product/src/main/java/com/example/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/product")
+//@RequestMapping("/api/product")
 @RestController
 public class ProductController {
 
